bd984706d4670039eec6898bf677f838f1db4caa
Miguelzuleta002 <miguel.zuleta@est.iudigital.edu.co>
2026-03-28 22:38:45 -0500

    Se crea el servicio para el despliegue del frontend

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,8 +1,10 @@
 import axios from 'axios';
 
-// Instancia configurada de Axios apuntando al backend local
+// Instancia configurada de Axios apuntando al backend local o de producción
+const apiEndpoint = import.meta.env.VITE_API_URL || 'http://localhost:4000/api';
+
 const api = axios.create({
-    baseURL: 'http://localhost:4000/api',
+    baseURL: apiEndpoint,
     headers: {
         'Content-Type': 'application/json'
     }
